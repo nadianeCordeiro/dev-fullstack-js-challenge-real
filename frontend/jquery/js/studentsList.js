50f6412d6935b307e80a5598116b8d201f20fb38
nadianeCordeiro <cordeironadiane@gmail.com>
2023-11-30 06:46:42 -0300

    importacao do jquery na lista de estudantes, montado requisicao get no endpoint students list, importacao do cors e exibicao dos studantes na UI - complemento

---
+++ frontend/jquery/js/studentsList.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    fetchStudentList();
+
+});
+
+function fetchStudentList(){
+    fetch("http://localhost:3000/students/list") /**fetch é do javascript  permite que se faça requisições para  apis  ou urls  , nesse caso estamos passando o endereço, onde possui a porta e o endpoint, isso é uma promesse para operações assincronas */
+    .then(function(response){
+        return response.json(); /**transformando a resposta do servidor em um json */
+    })
+    .then(function(data){ /**uma vez que houve o retorno do servidor e converteu para json ele vai excutar esse processo abaixo */
+     console.log(data);
+
+     const table = $("#studentList tbody"); /**criando uma const para  a tabela que foi criado no arquivo studentsList.html */
+     data.map(function(student) { /**percorrendo os dados retornados e montando a tabela  */
+        table.append(`
+                <tr>
+                    <td>${student.ra}</td>
+                    <td>${student.name}</td>
+                    <td>${student.cpf}</td>
+                    <td>
+                        <a href="#">Editar</a>
+                        <a href="#">Excluir</a>
+                    </td>
+                </tr>
+        
+        `);
+        
+     })
+    });
+}
+
